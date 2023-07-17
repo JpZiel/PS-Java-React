7cf7c0a9c7969c5fa87d740fa67ce27f52c98f57
JpZiel <joaopedromendes1@hotmail.com>
2023-07-16 23:28:10 -0300

    Camada Sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 CREATE TABLE conta
 (
-    id_conta IDENTITY NOT NULL PRIMARY KEY,
+    id IDENTITY NOT NULL PRIMARY KEY,
     nome_responsavel VARCHAR(50) NOT NULL
 );
 
@@ -16,11 +16,11 @@ CREATE TABLE transferencia
 
         CONSTRAINT FK_CONTA
         FOREIGN KEY (conta_id)
-        REFERENCES conta(id_conta)
+        REFERENCES conta(id)
 );
 
-INSERT INTO conta (id_conta, nome_responsavel) VALUES (1,'Fulano');
-INSERT INTO conta (id_conta, nome_responsavel) VALUES (2,'Sicrano');
+INSERT INTO conta (id, nome_responsavel) VALUES (1,'Fulano');
+INSERT INTO conta (id, nome_responsavel) VALUES (2,'Sicrano');
 
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (1,'2019-01-01 12:00:00+03',30895.46,'DEPOSITO', null, 1);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (2,'2019-02-03 09:53:27+03',12.24,'DEPOSITO', null,2);
